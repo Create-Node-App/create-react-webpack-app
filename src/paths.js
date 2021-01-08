b86a8e1d39bc2e78348454b554a368f9352b8468
ulises-jeremias <ulisescf.24@gmail.com>
2021-01-08 17:54:36 -0300

    Fix

--- src/paths.js
+++ src/paths.js
@@ -5,8 +5,8 @@ const gitCache = require('./git-tools');
 async function solveGitPath(addon) {
   const [gitPath, type] = addon.split('#type=');
   const [branch, ...url] = gitPath.split('@').reverse();
-  const id = new Buffer(gitPath).toString('base64');
-  const target = path.join(os.homedir(), '.crwp', 'crwp', id);
+  const id = Buffer.from(gitPath).toString('base64');
+  const target = path.join(os.homedir(), '.crwp', id);
   try {
     await gitCache({ git: url.reverse().join('@'), branch, target });
   } catch (err) {
